3ab708b498fea61dc65443885a6b8134522c778c
Yangsuwon <ysw7939@gmail.com>
2024-02-06 04:20:15 +0900

    refactor : Loca api path

--- src/main/java/geo/geoSystem/controller/LocaController.java
+++ src/main/java/geo/geoSystem/controller/LocaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/Loca-api")
+@RequestMapping("api/loca-api")
 @RequiredArgsConstructor
 public class LocaController {
     private final LocaService locaService;
